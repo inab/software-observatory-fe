a346b7579201dd71bcdd93495f221ede53445518
Eva Martin del Pico <eva.mdpico@gmail.com>
2022-05-25 15:37:16 +0200

    feat: overview plot

--- pages/OurData.vue
+++ pages/OurData.vue
@@ -1,11 +1,18 @@
 <template>
     <div>
         <v-row>
+            <v-col cols="12">
             <h1>Data Sources</h1>
+            </v-col>
             <MainCards />
         </v-row>
         <v-row>
-            <h1>Data Overview</h1>
+            <v-col cols="12">
+                <h1>Data Overview</h1>
+            </v-col>
+            <v-col>
+                <PlotOverview />
+            </v-col>
         </v-row>
     </div>
 </template>
